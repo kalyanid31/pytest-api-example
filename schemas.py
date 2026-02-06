1aafef80acf1c9f786e34a370f1e79be2e8117a7
kalyanid31 <kalyanid31@gmail.com>
2026-02-05 21:53:20 -0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,3 +1,6 @@
+# pet schemas
+# --------
+
 pet = {
     "type": "object",
     "required": ["name", "type"],
@@ -16,5 +19,26 @@ pet = {
             "type": "string",
             "enum": ["available", "sold", "pending"]
         },
+    }
+}
+
+# 2) *Optional* Consider creating an 'Order' model in schemas.py and validating it in the test
+# Order schemas
+# -------------------
+order = {
+    "type": "object",
+    "required": ["id", "pet_id", "status"],
+    "properties": {
+        "id": {"type": "string"},        # UUID
+        "pet_id": {"type": "integer"},
+        "status": {"type": "string", "enum": ["available", "pending", "sold"]}
+    }
+}
+
+patch_order_response = {
+    "type": "object",
+    "required": ["message"],
+    "properties": {
+        "message": {"type": "string"}
     }
 }
